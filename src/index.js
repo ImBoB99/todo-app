60889fc87677267d285c204434399574fd75e239
Dominik Ubuntu <119432963+ImBoB99@users.noreply.github.com>
2024-10-28 18:07:41 +0100

    add side nav and menu icons

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import "./styles.css";
 import { Todo } from "./modules/Todo";
 import { TodoGroup } from "./modules/TodoGroup";
 
-const todo1 = new Todo ("Walk Dog", "Take dog for a walk", "2014-02-8", true)
-const todo2 = new Todo ("Brush Dog", "Brush the dog", "2024-04-6", false)
-const todo3 = new Todo ("Wash Dog", "Wash the dog", "2033-22-12", false)
+const todo1 = new Todo ("Walk Dog", "Take dog for a walk", "2014-02-8", "High")
+const todo2 = new Todo ("Brush Dog", "Brush the dog", "2024-04-6", "Medium")
+const todo3 = new Todo ("Wash Dog", "Wash the dog", "2033-22-12", "Low")
 const project = new TodoGroup ("Hope")
 
 project.addTodo(todo1)
